201d25933f3b01bffe07cab9704665d429eb103f
StefanySantos <stefanyjenifer1@hotmail.com>
2021-11-11 20:48:39 -0300

    exericio2

---
+++ aula_04/exercicio_2.js
@@ -0,0 +1,14 @@
+const roupas = {
+    chapeu: 'bone',
+    camiseta: 'camiseta do batman',
+    sapatos: 'tenis da nike'
+}
+
+console.log(roupas);
+
+//adc propriedade 
+roupas.valorChapeu = 54.0;
+console.log(roupas);
+
+roupas.valorCamiseta = 85;
+console.log(roupas);
