bce4fb82f34640d53885b363e3655d73e3bc9e9c
StefanySantos <stefanyjenifer1@hotmail.com>
2021-11-11 19:46:13 -0300

    exercicios

---
+++ aula_2.js
@@ -0,0 +1,18 @@
+let nome = "stefany";
+let segundoNome = "jenifer";
+
+console.log(nome);
+
+/*tipos de declaração
+    var = acessado de qualquer lugar
+    let = acessado dentro do escopo
+    const = 
+
+*/
+
+let estado = 'SP';
+
+function imprimeEstado() {
+    let cidade = 'São Paulo';
+    console.log(estado);
+}
---
+++ aula_3.js
@@ -0,0 +1,7 @@
+const hojeVaiChover = true;
+
+if (hojeVaiChover) {
+    console.log("leve seu guarda chuva");
+} else {
+    console.log("fique tranquilo, solzão")
+}
